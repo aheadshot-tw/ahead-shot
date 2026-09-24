00f9aab21e09e0a60647d7de28a04f219c088dd0
aheadshot-tw <aheadshot.tw@gmail.com>
2026-09-24 17:18:42 +0800

    Set LINE booking URL

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 // 只需要修改下方三個網址，其餘檔案不用更動。
-const LINE_URL = "https://example.com/line";
+const LINE_URL = "https://lin.ee/UPDY1mC";
 const FACEBOOK_URL = "https://example.com/facebook";
 const INSTAGRAM_URL = "https://example.com/instagram";
-
