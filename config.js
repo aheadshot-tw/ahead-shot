4157a1aece8d3b25a9958156040fd338d3d173fa
aheadshot-tw <aheadshot.tw@gmail.com>
2026-09-25 23:55:52 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // 只需要修改下方三個網址，其餘檔案不用更動。
 const LINE_URL = "https://lin.ee/UPDY1mC";
-const FACEBOOK_URL = "https://example.com/facebook";
+const FACEBOOK_URL = "https://www.facebook.com/AHEADSHOT.TW/";
 const INSTAGRAM_URL = "https://example.com/instagram";
